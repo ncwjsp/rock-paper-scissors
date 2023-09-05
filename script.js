39912c97fc53d6cca4fea105aae7b2fda2f90e99
ncwjsp <nc.wjsp@gmail.com>
2023-09-06 01:44:06 +0700

    Finish the game

--- script.js
+++ script.js
@@ -2,10 +2,59 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   switch (choice) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Paper";
+      return "paper";
     case 2:
-      return "Scissors";
+      return "scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection == computerSelection) {
+    return "Tied!";
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "Computer";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "Player";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "Player";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "Computer";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "Computer";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "Player";
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (i = 0; i < 5; i++) {
+    const playerSelection = prompt(
+      "Enter your choice Rock, Paper or Scissors: "
+    );
+    const computerSelection = getComputerChoice();
+    if (playRound(playerSelection, computerSelection) == "Player") {
+      ++playerScore;
+      console.log("Player !");
+    } else if (playRound(playerSelection, computerSelection) == "Computer") {
+      ++computerScore;
+      console.log("Computer !");
+    } else {
+      console.log("Tied !");
+    }
+  }
+  console.log("=======================================");
+  if (playerScore > computerScore) {
+    console.log(`Player won with score ${playerScore} to ${computerScore}`);
+  } else if (playerScore < computerScore) {
+    console.log(`Player lost with score ${playerScore} to ${computerScore}`);
+  } else {
+    console.log(`Tied game !`);
+  }
+}
+
+game();
